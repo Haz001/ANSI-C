7c73ab08d2db38270d565732a7a96bdea8d22c61
root <root@harrysy.red>
2023-09-20 13:47:25 +0000

    +[ More comments, better range and headers ]
    main.c:
    	- more comments throughout code
    	- range is now from -100 to 100 in steps of 10
    	- Collumns now have headers

--- 1/2/main.c
+++ 1/2/main.c
@@ -2,19 +2,27 @@
 
 main()
 {
-	/* variables to hold current version of temp */
+	/* farh holds the current temp to be converted, celsius holds the
+	   converted temperature*/
 	int farh, celsius;
-	/* range of conversions */
+
+	/* range of conversions for farh*/
 	int start, stop, step;
 
-	start = 0;
-	stop = 300;
-	step = 20;
+	start = -100;
+	stop = 100;
+	step = 10;
 
+	printf("°F\t°C\n");
+	/* farh acts as counter */
 	farh = start;
-	while (farh <= stop){
+	/* loop till counter (farh) is bigger than stop, inclusive range */
+	while (farh <= stop)
+	{ 
+		/* convert farh to celcius */
 		celsius =  ((farh - 32) * 5) / 9;
 		printf("%d\t%d\n",farh, celsius);
+		/* increemnt counter (farh) */
 		farh = farh + step;
 	}
 }
