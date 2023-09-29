c77fb6c212b92dc60c0c3a0e89cfa28a53739bc0
Harry S <git@harrysy.red>
2023-09-29 17:41:34 +0000

    + [ Convert whiles to for and improved makefile]
    Makefile:
    	- Added clean function
    	- Used file names for targets
    	- Added defaults instead of using top
    
    c2f.c f2c.c:
    	- Converted it to using for loop instead of while loop
    	- Reduced variable count

---
+++ 1/3/c2f.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+	int celsius;
+	printf("  °C\t    °F\n");
+	for (celsius = -100;celsius <= 100;celsius += 10)
+	{ 
+		printf("%4d\t%6.0f\n", celsius, (celsius * (9.0 / 5.0)) + 32);
+	}
+}
---
+++ 1/3/f2c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+	int farh;
+	printf("  °F\t    °C\n");
+	for (farh = -100; farh <= 100; farh = farh + 10)
+	{ 
+		printf("%4d\t%6.1f\n", farh, (farh-32) * (5.0/9.0));
+	}
+}
