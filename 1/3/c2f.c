c4cbd1e0e5aeff79cfcd39840c5adb895f67563a
Harry S <git@harrysy.red>
2023-09-29 17:57:17 +0000

    + [ Reversed for loop and optimized]
    *.c:
    	- Reversed for loop so 100 starts at the top
    f2c.c:
    	- changed operander in increment section of for loop

--- 1/3/c2f.c
+++ 1/3/c2f.c
@@ -4,7 +4,7 @@ main()
 {
 	short celsius;
 	printf("  °C\t    °F\n");
-	for (celsius = -100;celsius <= 100;celsius += 10)
+	for (celsius = 100;celsius >= -100;celsius -= 10)
 	{ 
 		printf("%4d\t%6.0f\n", celsius, (celsius * (9.0 / 5.0)) + 32);
 	}
--- 1/3/f2c.c
+++ 1/3/f2c.c
@@ -4,7 +4,7 @@ main()
 {
 	short farh;
 	printf("  °F\t    °C\n");
-	for (farh = -100; farh <= 100; farh = farh + 10)
+	for (farh = 100; farh >= -100; farh -= 10)
 	{ 
 		printf("%4d\t%6.1f\n", farh, (farh-32) * (5.0/9.0));
 	}
